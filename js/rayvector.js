ad5117c250dae38c7eb657d31498da11b86af350
rkibria <raihan@kibria.de>
2017-10-07 15:23:44 +0100

    copy()

--- js/rayvector.js
+++ js/rayvector.js
@@ -12,6 +12,13 @@ Vector3.prototype.toString = function vector3ToString() {
 	return this.x + ', ' + this.y + ', ' + this.z;
 };
 
+Vector3.prototype.copy = function(v) {
+	this.x = v.x;
+	this.y = v.y;
+	this.z = v.z;
+	return this;
+};
+
 Vector3.prototype.add = function(v) {
 	this.x += v.x;
 	this.y += v.y;
