373ef01d817c6dd67275a83594b77157641c0cfd
Raihan Kibria <raihan@kibria.de>
2018-03-30 21:32:42 +0100

    Clean up

--- webassembly/fractal/main.js
+++ webassembly/fractal/main.js
@@ -1,7 +1,5 @@
 "use strict";
 
-const START_TIME = performance.now();
-
 function js_mandelbrot(cx, cy, range) {
     let k = 0;
     let zx = 0;
@@ -20,30 +18,28 @@ function js_mandelbrot(cx, cy, range) {
 
 function js_render(canvas) {
     // https://stackoverflow.com/questions/4032179/how-do-i-get-the-width-and-height-of-a-html5-canvas
-    const SCREEN_WIDTH = canvas.scrollWidth;
-    const SCREEN_HEIGHT = canvas.scrollHeight;
+    const width = canvas.scrollWidth;
+    const height = canvas.scrollHeight;
 
     const ctx = canvas.getContext("2d");
     ctx.fillStyle="black";
-    ctx.fillRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
-    const imgData = ctx.getImageData(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+    ctx.fillRect(0, 0, width, height);
+    const imgData = ctx.getImageData(0, 0, width, height);
 
     const range = 255;
-    const animTime = 0; // performance.now() - START_TIME;
-    const zoomTimed = (1 + Math.cos(animTime/1000)) / 2;
-    const zoom = SCREEN_WIDTH * (0.2 + zoomTimed * 20);
-    // const zoom = SCREEN_WIDTH / 3 * 1.2;
-    const offsetX = Math.cos(animTime/1000)/10;
+    const zoom = width * (0.2);
+
+    const offsetX = 0;
     const offsetY = offsetX * 1.2;
     const centerX = 0.3 + offsetX;
     const centerY = 0 + offsetY;
 
     for (let i = 0; i < imgData.data.length; i += 4) {
-        const x = (i / 4) % SCREEN_WIDTH;
-        const y = SCREEN_HEIGHT - (i / (4 * SCREEN_HEIGHT));
+        const x = (i / 4) % width;
+        const y = height - (i / (4 * height));
 
-        const cx = (centerX - SCREEN_WIDTH/2/zoom) + x / zoom;
-        const cy = (centerY - SCREEN_WIDTH/2/zoom) + y / zoom;
+        const cx = (centerX - width/2/zoom) + x / zoom;
+        const cy = (centerY - width/2/zoom) + y / zoom;
 
         const k = js_mandelbrot(cx, cy, range);
 
@@ -58,12 +54,6 @@ function js_render(canvas) {
         else
             color.z = k;
 
-        if (cx*cx+cy*cy < 0.0001) {
-            color.x = 1;
-            color.y = 1;
-            color.z = 1;
-        }
-
         imgData.data[i] = Math.max (0, Math.min (255, color.x * 255));
         imgData.data[i+1] = Math.max (0, Math.min (255, color.y * 255));
         imgData.data[i+2] = Math.max (0, Math.min (255, color.z * 255));
@@ -73,16 +63,16 @@ function js_render(canvas) {
 }
 
 function c_render(canvas) {
-    const SCREEN_WIDTH = canvas.scrollWidth;
-    const SCREEN_HEIGHT = canvas.scrollHeight;
+    const width = canvas.scrollWidth;
+    const height = canvas.scrollHeight;
 
     const ctx = canvas.getContext("2d");
     ctx.fillStyle="black";
-    ctx.fillRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
-    const imgData = ctx.getImageData(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+    ctx.fillRect(0, 0, width, height);
+    const imgData = ctx.getImageData(0, 0, width, height);
 
     let t0 = performance.now();
-    const imgPointer = _c_render_mandelbrot(SCREEN_WIDTH, SCREEN_HEIGHT);
+    const imgPointer = _c_render_mandelbrot(width, height);
     let t1 = performance.now();
     console.log("c_render_mandelbrot time: " + (t1 - t0) + " ms");
 
@@ -119,8 +109,6 @@ function captionText(canvas, txt) {
 }
 
 (function drawFrame () {
-    // window.requestAnimationFrame(drawFrame, canvas);
-
     const wasmCanvas = document.getElementById("wasmCanvas");
     const t0 = performance.now();
     c_render (wasmCanvas);
@@ -132,5 +120,4 @@ function captionText(canvas, txt) {
     js_render (jsCanvas);
     const t1js = performance.now();
     captionText(jsCanvas, "JS render time: " + (t1js - t0js) + " ms");
-
 }());
