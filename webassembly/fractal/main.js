89a56747bc24e95c0910461484b7b459d2aaaa2b
Raihan Kibria <raihan@kibria.de>
2018-03-30 20:56:47 +0100

    Tab to space

--- webassembly/fractal/main.js
+++ webassembly/fractal/main.js
@@ -26,47 +26,47 @@ function js_mandelbrot(cx, cy, range) {
 }
 
 function js_render(imgData) {
-	const range = 255;
-	const animTime = performance.now() - START_TIME;
-	const zoomTimed = (1 + Math.cos(animTime/1000)) / 2;
-	const zoom = SCREEN_WIDTH * (0.2 + zoomTimed * 20);
-	// const zoom = SCREEN_WIDTH / 3 * 1.2;
-	const offsetX = Math.cos(animTime/1000)/10;
-	const offsetY = offsetX * 1.2;
-	const centerX = 0.3 + offsetX;
-	const centerY = 0 + offsetY;
+    const range = 255;
+    const animTime = performance.now() - START_TIME;
+    const zoomTimed = (1 + Math.cos(animTime/1000)) / 2;
+    const zoom = SCREEN_WIDTH * (0.2 + zoomTimed * 20);
+    // const zoom = SCREEN_WIDTH / 3 * 1.2;
+    const offsetX = Math.cos(animTime/1000)/10;
+    const offsetY = offsetX * 1.2;
+    const centerX = 0.3 + offsetX;
+    const centerY = 0 + offsetY;
 
-	for (let i = 0; i < imgData.data.length; i += 4) {
-		const x = (i / 4) % SCREEN_WIDTH;
-		const y = SCREEN_HEIGHT - (i / (4 * SCREEN_HEIGHT));
+    for (let i = 0; i < imgData.data.length; i += 4) {
+        const x = (i / 4) % SCREEN_WIDTH;
+        const y = SCREEN_HEIGHT - (i / (4 * SCREEN_HEIGHT));
 
-		const cx = (centerX - SCREEN_WIDTH/2/zoom) + x / zoom;
-		const cy = (centerY - SCREEN_WIDTH/2/zoom) + y / zoom;
+        const cx = (centerX - SCREEN_WIDTH/2/zoom) + x / zoom;
+        const cy = (centerY - SCREEN_WIDTH/2/zoom) + y / zoom;
 
         const k = js_mandelbrot(cx, cy, range);
 
-		const color = {x: 0, y: 0, z: 0};
+        const color = {x: 0, y: 0, z: 0};
 
-		const firstRange = 0.1;
-		const secondRange = 0.9;
-		if (k <= firstRange)
-			color.x = Math.sqrt(k / firstRange);
-		else if (k <= secondRange)
-			color.y = Math.sqrt(k / secondRange);
-		else
-			color.z = k;
+        const firstRange = 0.1;
+        const secondRange = 0.9;
+        if (k <= firstRange)
+            color.x = Math.sqrt(k / firstRange);
+        else if (k <= secondRange)
+            color.y = Math.sqrt(k / secondRange);
+        else
+            color.z = k;
 
-		if (cx*cx+cy*cy < 0.0001) {
-			color.x = 1;
-			color.y = 1;
-			color.z = 1;
-		}
+        if (cx*cx+cy*cy < 0.0001) {
+            color.x = 1;
+            color.y = 1;
+            color.z = 1;
+        }
 
-		imgData.data[i] = Math.max (0, Math.min (255, color.x * 255));
-		imgData.data[i+1] = Math.max (0, Math.min (255, color.y * 255));
-		imgData.data[i+2] = Math.max (0, Math.min (255, color.z * 255));
-		imgData.data[i+3] = 255;
-	}
+        imgData.data[i] = Math.max (0, Math.min (255, color.x * 255));
+        imgData.data[i+1] = Math.max (0, Math.min (255, color.y * 255));
+        imgData.data[i+2] = Math.max (0, Math.min (255, color.z * 255));
+        imgData.data[i+3] = 255;
+    }
 }
 
 // Get canvas bitmap
@@ -94,12 +94,12 @@ function c_render(imgData) {
 }
 
 (function drawFrame () {
-	// window.requestAnimationFrame(drawFrame, canvas);
+    // window.requestAnimationFrame(drawFrame, canvas);
 
-	const t0 = performance.now();
-	c_render (imgData);
-	const t1 = performance.now();
-	console.log("total render time: " + (t1 - t0) + " ms");
+    const t0 = performance.now();
+    c_render (imgData);
+    const t1 = performance.now();
+    console.log("total render time: " + (t1 - t0) + " ms");
 
-	ctx.putImageData(imgData, 0, 0);
+    ctx.putImageData(imgData, 0, 0);
 }());
